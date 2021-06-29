6c8e26a80864943c91e0c24e6f93615cd40c96fb
Rubèn Nieto <ruben.np@me.com>
2021-06-29 11:27:29 +0200

    Fi n1

--- src/films.js
+++ src/films.js
@@ -1,34 +1,55 @@
+// import { movies } from './data.js';
+
 // Exercise 1: Get the array of all directors.
-function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
-  return result;
-}
+const getAllDirectors = array => 
+  array.map(el => el.director);
 
 // Exercise 2: Get the films of a certain director
-function getMoviesFromDirector(array, director) {
- 
-}
+const getMoviesFromDirector = (array, director) => 
+  array.filter(el => el.director === director);
 
 // Exercise 3: Calculate the average of the films of a given director.
-function moviesAverageOfDirector(array, director) {
-  
-}
+const moviesAverageOfDirector = (array, director) => 
+  getMoviesFromDirector(array, director)
+  .map(el => el.score)
+  .reduce((total, actual, i, array) => (total + actual)/array.length);
 
 // Exercise 4:  Alphabetic order by title 
-function orderAlphabetically(array) {
-  
-}
+const orderAlphabetically = array =>
+  array
+  // només el títol
+  .map(el => el.title)                             
+  // ordenat alfabèticament   
+  .sort((a, b) => (a === b) ? 0 : (a > b) ? 1 : -1)
+  // retorna només els 20 primers
+  .filter((v, i) => i < 20);                                   
 
-// Exercise 5: Order by year, ascending
-function orderByYear() {
-
-}
+  // Exercise 5: Order by year, ascending
+const orderByYear = array => 
+  array
+    // només any i títol en objecte
+    .map(el => { return { "year": el.year, "title": el.title }; })
+    // ordena per any
+    .sort((a, b) => a.year - b.year)
+    // anys iguals, ordena per títol alfabèticament
+    .sort((a, b) => (a.year === b.year) ? (a.title > b.title) ? 1 : -1 : 0);
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
+const moviesAverageByCategory = (array, category) => {
+  let noScore = 0;
 
-}
+  return array
+    .filter(el => el.genre.includes(category))
+    .map(el => el.score)
+    .reduce((total, actual, i, array) => {
+      if (!actual) {
+        ++noScore;
+        return total;
+      }
+      return (total + actual)/array.length-noScore;
+    });
+  };
+// console.log(moviesAverageByCategory(movies, 'Mystery'));
 
 // Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes() {
